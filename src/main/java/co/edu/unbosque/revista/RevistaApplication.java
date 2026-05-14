2b60b918ecf3c39917b4dbe7bb5f840aa02113e3
sanchezgina0312 <sanchez.gina0312@gmail.com>
2026-05-14 12:01:13 -0500

    Initial Spring Boot project with Maven wrapper
    Add project skeleton and build tooling: Maven wrapper (.mvn/wrapper, mvnw, mvnw.cmd) and pom.xml (Java 21, war packaging, Spring Boot dependencies). Include repository config files (.gitignore, .gitattributes), application entrypoint (RevistaApplication), ServletInitializer for WAR deployment, application.properties, and a basic Spring boot context-load test.

---
+++ src/main/java/co/edu/unbosque/revista/RevistaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.revista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/revista/RevistaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.revista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
